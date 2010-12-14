fbc98a3df0719a5cbffa97eda0241af1a4ccb83a
nazri <ayiehere@gmail.com>
2010-12-14 16:18:17 +0800

    App.scala: Formatting

--- src/main/scala/org/guigit/App.scala
+++ src/main/scala/org/guigit/App.scala
@@ -13,20 +13,28 @@ import scala.collection.jcl.MutableIterator.Wrapper
 
 object App extends Application
 {
-  implicit def javaIteratorToScalaIterator[A](it : java.util.Iterator[A]) = new Wrapper(it)
-
+  implicit def javaIteratorToScalaIterator[A](it : java.util.Iterator[A])
+                                                              = new Wrapper(it)
 
   try {
     var builder = new FileRepositoryBuilder()
-    var repository = builder.setGitDir(new File("/home/nazri/src/git/.git")).readEnvironment()
-    .findGitDir()
-    .build();
+    var repository = builder.setGitDir(new File("/home/nazri/src/git/.git"))
+                            .readEnvironment()
+                            .findGitDir()
+                            .build();
 
     var g = new Git(repository);
     var log = g.log().add(repository.resolve("HEAD"));
 
     var a = 1
-    log.call().iterator().foreach((commit:RevCommit) => { if (a < 2) { println(commit.getFullMessage()) }; a += 1 } )
+    log.call().iterator().foreach(
+                            (commit:RevCommit) => {
+                              if (a < 2) {
+                                println(commit.getFullMessage())
+                              }
+                              a += 1
+                            }
+                          )
   } catch {
     case e:IOException => println("guigit: " + e.getStackTrace())
     case e:NoHeadException => println("guigit: " + e.getStackTrace())
