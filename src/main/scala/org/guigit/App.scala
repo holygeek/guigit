e7df3856aa307a70ee0f828c75cb5d70ccdac5ac
nazri <ayiehere@gmail.com>
2010-12-22 17:35:54 +0800

    Prefuse visualization - circle nodes with directed edges

--- src/main/scala/org/guigit/App.scala
+++ src/main/scala/org/guigit/App.scala
@@ -3,6 +3,10 @@ package org.guigit
 import java.io.File
 import java.io.IOException
 import java.awt.Toolkit
+import java.awt.geom.Rectangle2D
+import java.awt.Shape
+import java.awt.Color
+import java.awt.geom.Point2D
 import javax.swing.JFrame
 import javax.swing.WindowConstants
 import javax.swing.SwingUtilities
@@ -17,15 +21,34 @@ import prefuse.Display
 import prefuse.data.Graph
 import prefuse.data.Table
 import prefuse.Visualization
+import prefuse.render.LabelRenderer
+import prefuse.render.ShapeRenderer
+import prefuse.render.AbstractShapeRenderer
+import prefuse.render.DefaultRendererFactory
+import prefuse.action.assignment.ColorAction
+import prefuse.visual.VisualItem
+import prefuse.util.ColorLib
+import prefuse.action.ActionList
+import prefuse.action.assignment.ShapeAction
+import prefuse.activity.Activity
+import prefuse.action.layout.graph.ForceDirectedLayout
+import prefuse.action.RepaintAction
+import prefuse.controls.DragControl
+import prefuse.controls.PanControl
+import prefuse.controls.ZoomControl
+import prefuse.visual.expression.InGroupPredicate
 
 import scala.collection.JavaConversions._
+import scala.collection.mutable.HashMap
 
 object App extends Application
 {
 
   var good = false
   val nodesTable = new Table()
-  nodesTable.addColumn("refcommit", classOf[RevCommit])
+  nodesTable.addColumn("revcommit", classOf[RevCommit])
+  val rowIdFor = new HashMap[RevCommit, Int]
+  val edgeMap = new HashMap[RevCommit, Array[RevCommit]]
 
   try {
     var builder = new FileRepositoryBuilder()
@@ -34,25 +57,36 @@ object App extends Application
                             .build();
 
     val allrefs = repository.getAllRefs()
+    var g = new Git(repository);
+    var log = g.log()
+
     allrefs.keySet().foreach(
                       (refname:String) => {
-                        println("Ref: " + refname + ": " + allrefs.get(refname))
+                        println("Ref: "
+                                + refname
+                                + ": "
+                                + allrefs.get(refname))
+                        log.add(repository.resolve(refname))
                       }
                     )
 
-    var g = new Git(repository);
-    var log = g.log().add(repository.resolve("HEAD"))
 
 
     var a = 1
-    log.call().iterator().foreach(
-                            (commit:RevCommit) => {
-                              if (a < 5) {
-                                println(commit.getFullMessage())
-                              }
-                              a += 1
-                            }
-                          )
+    log.call()
+        .iterator()
+        .foreach(
+          (commit:RevCommit) => {
+            if (a < 5) {
+              println(commit.getFullMessage())
+            }
+            a += 1
+            val row = nodesTable.addRow()
+            nodesTable.set(row, "revcommit", commit)
+            rowIdFor += commit -> row
+            edgeMap += commit -> commit.getParents()
+          }
+        )
     good = true
   } catch {
     case e:IOException => println("guigit: " + e.getStackTrace())
@@ -63,18 +97,78 @@ object App extends Application
     exit()
 
   val graph = new Graph(nodesTable, true /* directed */)
+  val edges = graph.getEdgeTable()
+  edgeMap.foreach((parentMap) => {
+        val commitId = rowIdFor.getOrElse(parentMap._1, -1)
+        parentMap._2.foreach((commit) => {
+            val parentId:Int = rowIdFor.getOrElse(commit, -1)
+            graph.addEdge(commitId, parentId)
+        })
+    })
 
   val vis = new Visualization()
+  vis.add("graph", graph)
+
+  val rf = new DefaultRendererFactory()
+  val nodeRenderer = new ShapeRenderer(10)
+  rf.setDefaultRenderer(nodeRenderer)
+  vis.setRendererFactory(rf)
+  //val textColorAction = new ColorAction("graph.nodes",
+  //          VisualItem.TEXTCOLOR,
+  //          ColorLib.gray(0))
+
+  // Nodes
+  var nodeShapeAction = new ShapeAction("graph.nodes")
+  nodeShapeAction.setDefaultShape(prefuse.Constants.SHAPE_ELLIPSE)
+  var nodeStrokeAction = new ColorAction("graph.nodes",
+         VisualItem.STROKECOLOR,
+         ColorLib.color(Color.blue))
+  var nodeFillColorAction = new ColorAction("graph.nodes",
+         VisualItem.FILLCOLOR,
+         ColorLib.color(Color.orange))
+
+  // Edges
+  var edgeColorAction = new ColorAction("graph.edges",
+         VisualItem.STROKECOLOR,
+         ColorLib.gray(200))
+  var arrowHeadColorAction = new ColorAction("graph.edges",
+         VisualItem.FILLCOLOR,
+         ColorLib.gray(200))
+
+  var color = new ActionList()
+  //color.add(textColorAction)
+  color.add(nodeStrokeAction)
+  color.add(nodeFillColorAction)
+  color.add(edgeColorAction)
+  color.add(arrowHeadColorAction)
+
+  var layout = new ActionList(Activity.INFINITY);
+  layout.add(new ForceDirectedLayout("graph"));
+  layout.add(new RepaintAction());
+
+  vis.putAction("shape", nodeShapeAction)
+  vis.putAction("color", color);
+  vis.putAction("layout", layout);
+
   val display = new Display(vis)
+  // drag individual items around
+  display.addControlListener(new DragControl());
+  // pan with left-click drag on background
+  display.addControlListener(new PanControl());
+  // zoom with right-click drag
+  display.addControlListener(new ZoomControl());
 
   val frame = new JFrame("GuiGit")
   frame.add(display)
   val screenSize = Toolkit.getDefaultToolkit().getScreenSize()
-  val height = screenSize.height / 1.25
-  frame.setSize(800, height.intValue())
-  frame.setBackground(java.awt.Color.blue)
+  frame.setSize(800, (screenSize.height/1.25).intValue)
   frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
   frame.setLocationRelativeTo(null)
 
   frame.setVisible(true)
+  val center = new Point2D.Double(0, 0)
+  display.panTo(center)
+  vis.run("shape")
+  vis.run("color")
+  vis.run("layout")
 }
