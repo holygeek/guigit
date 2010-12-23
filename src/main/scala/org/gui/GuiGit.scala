cb125c0ccc830ec90e808a8a5ca46c4a68b95f49
nazri <ayiehere@gmail.com>
2010-12-23 17:58:38 +0800

    add org.gui.GuiGit

---
+++ src/main/scala/org/gui/GuiGit.scala
@@ -0,0 +1,22 @@
+package org.gui
+
+import prefuse.Display
+
+import java.awt.geom.Point2D
+import java.awt.Toolkit
+import javax.swing.JFrame
+import javax.swing.WindowConstants
+
+class GuiGit(display:Display) {
+  val frame = new JFrame("GuiGit")
+  frame.add(display)
+  val screenSize = Toolkit.getDefaultToolkit().getScreenSize()
+  frame.setSize(800, (screenSize.height/1.25).intValue)
+  frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+  frame.setLocationRelativeTo(null)
+
+  frame.setVisible(true)
+
+  val center = new Point2D.Double(0, 0)
+  display.panTo(center)
+}
