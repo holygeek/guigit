19c81f8933a733198df0d6cac8f5099bace2c054
nazri <ayiehere@gmail.com>
2011-01-03 16:08:30 +0800

    GitGraphLayout: NodeDetailWrapper class to help implicit retrieval of columns

--- src/main/scala/org/visual/layout/GitGraphLayout.scala
+++ src/main/scala/org/visual/layout/GitGraphLayout.scala
@@ -1,12 +1,14 @@
 package org.visual.layout
 
 import scala.collection.mutable.HashMap
+import scala.collection.JavaConversions._
 
 import prefuse.data.tuple.TupleSet
 import prefuse.data.Graph
 import prefuse.action.layout.Layout
 import prefuse.data.Node
 import prefuse.visual.NodeItem
+import prefuse.visual.VisualItem
 
 import org.eclipse.jgit.revwalk.RevCommit
 import org.eclipse.jgit.revwalk.RevWalk
@@ -14,9 +16,24 @@ import org.eclipse.jgit.revwalk.RevWalk
 import org.domain.GitGraph
 
 class GitGraphLayout(gitGraph: GitGraph) extends Layout {
+  class NodeDetail(val node: Node) {
+    def x = node.get("x").asInstanceOf[Int]
+    def y = node.get("y").asInstanceOf[Int]
+    def yOffset = node.get("yOffset").asInstanceOf[Int]
+    def depth = node.get("depth").asInstanceOf[Int]
+    def x_= (x:Int) = node.set("x", x)
+    def y_= (y:Int) = node.set("y", y)
+    def yOffset_= (yOffset:Int) = node.set("yOffset", yOffset)
+    def depth_= (depth: Int) = node.set("depth", depth)
+  }
+  implicit def nodeDetailWrapper(node: Node) = new NodeDetail(node)
+  implicit def nodeDetailWrapper(item: VisualItem) = new NodeDetail(item.asInstanceOf[NodeItem])
+
   private val ITEMGAP = org.visual.Constants.NODE_SIZE * 4
   private val alreadyPositioned = new HashMap[RevCommit, Boolean] 
+  private val hasDepth = new HashMap[RevCommit, Boolean]
   private var gridList = new HashMap[Int, Int]()
+
   def firstPass(): Unit = {
     gitGraph.branches.foreach(objectId => {
       try {
@@ -34,14 +51,14 @@ class GitGraphLayout(gitGraph: GitGraph) extends Layout {
   // TODO use higher order function for going over first and
   // second pass
   override def run(frac: Double): Unit = {
-    println("Firstpass")
+    print("Firstpass")
     firstPass()
-    println("Firstpass done")
-    println("2nd pass")
+    println(" done")
+    print("2nd pass")
     gitGraph.branches.foreach(objectId => {
       try {
         val commit = gitGraph.revWalk.parseCommit(objectId)
-        setXYPosition(commit, 0)
+        setDepth(commit, 0)
       } catch {
         case e: Exception => {
           println("guigit:")
@@ -49,64 +66,62 @@ class GitGraphLayout(gitGraph: GitGraph) extends Layout {
         }
       }
     })
-    println("2nd pass done")
+    println(" done")
+    print("3rd pass")
+    setXYPosition()
+    println(" done")
   }
 
-  private def moveDown(commit: RevCommit, row: Int): Any = {
+  private def setXYPosition() {
+    m_vis.items("graph.nodes").foreach(
+      (obj:Any) => {
+        val item = obj.asInstanceOf[VisualItem]
+        val yOffset = item.yOffset
+        val depth = item.depth + yOffset
+        setX(item, null, item.x * ITEMGAP)
+        setY(item, null, (item.y + depth) * ITEMGAP)
+      }
+    )
+  }
 
+  private def setDepth(commit: RevCommit, currDepth: Int): Any = {
     val node = gitGraph.getNode(commit)
+    if (hasDepth.getOrElse(commit, false)) {
+      val depth = node.depth
+      if (depth < currDepth) {
+        node.depth =  currDepth - node.y
+      }
+      return
+    }
 
-    val col = gridList.getOrElse(row - 1, 0)
-    gridList(row) = col + 1
-    node.set("y", row)
+    node.depth = currDepth
+    hasDepth(commit) = true
 
     val parents = commit.getParents()
     if (parents == null)
       return
 
-    parents.foreach(
-              (commit: RevCommit) => {
-                      val cmt = commit
-                      // val cmt = gitGraph.revWalk.parseCommit(commit)
-                      if (alreadyPositioned.getOrElse(cmt, false))
-                        moveDown(cmt, row)
-              }
-           )
-  }
-
-  private def setXYPosition(commit: RevCommit, currDown: Int): Any = {
-    val node = gitGraph.getNode(commit)
-    val item = m_vis.getVisualItem("graph.nodes", node)
-    val yOffset = item.get("yOffset").asInstanceOf[Int]
-
-    val nextDown = currDown + yOffset
-    setX(item, null, item.get("x").asInstanceOf[Int] * ITEMGAP)
-    setY(item, null, (item.get("y").asInstanceOf[Int] + nextDown) * ITEMGAP)
-    val parents = commit.getParents()
-    if (parents == null) {
-      return
-    }
+    val nextDepth = currDepth + node.yOffset
 
     parents.foreach(
               (commit: RevCommit)
-                  => setXYPosition(commit, nextDown)
+                  => setDepth(commit, nextDepth)
     )
   }
 
   private def setPosition(commit: RevCommit, row: Int, minCol: Int): Any = {
     val node = gitGraph.getNode(commit)
     if (alreadyPositioned.getOrElse(commit, false)) {
-      val currRow = node.get("y").asInstanceOf[Int]
-      if (currRow <= row) {
-        val yOffset = row - currRow
-        node.set("yOffset", yOffset)
-      }
+      if (node.y <= row)
+        node.yOffset= row - node.y
       return
     }
 
-    var col = gridList.getOrElse(row, minCol)
-    node.set("x", col)
-    node.set("y", row)
+    var col = gridList.getOrElse(row, 0)
+    if (col < minCol)
+      col = minCol
+    node.x = col
+    node.y = row
     gridList(row) = col + 1
     alreadyPositioned(commit) = true
 
